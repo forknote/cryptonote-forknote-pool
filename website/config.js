c106f60791e7665aac82e4bb6aef3c5a20f5c177
BBSCoin Developer <43017551+bbscoindev@users.noreply.github.com>
2018-05-04 21:23:12 -0700

    Remove pools that aren't up to date

--- website/config.js
+++ website/config.js
@@ -20,15 +20,13 @@ var themeCss = "themes/default-theme.css";
 
 var networkStat = {
     "bbs": [
-        ['pool.bbscoin.xyz', 'http://pool.bbscoin.xyz:8117'],
-        ['bbs.pool.pilbeams.net', 'http://bbs.pool.pilbeams.net:8111'],
-        ['bbs.pool.ahka.net', 'http://bbs.pool.ahka.net:8115'],
-	['bbs.dreampool.info', 'https://bbs.dreampool.info/api/'],
-	['cryptoknight.cc/bbs', 'http://78.46.85.142:2778'],
-	['bbs.gonspool.com', 'http://bbs.gonspool.com:8117'],
-	['pool-marine.ga/bbs', 'http://pool-marine.ga:8028'],
-	['bbs.howpool.com', 'http://bbs.howpool.com:8117'],
-	['bbscoin-pool.org', 'http://bbscoin-pool.org:8107'],
-	['149.129.129.195', 'http://149.129.129.195:8117']
+		['pool.bbscoin.xyz', 'http://pool.bbscoin.xyz:8117'],
+		['bbs.dreampool.info', 'https://bbs.dreampool.info/api/'],
+		['cryptoknight.cc/bbs', 'http://78.46.85.142:2778'],
+		['bbs.gonspool.com', 'http://bbs.gonspool.com:8117'],
+		['pool-marine.ga/bbs', 'http://pool-marine.ga:8028'],
+		['bbs.howpool.com', 'http://bbs.howpool.com:8117'],
+		['bbscoin-pool.org', 'http://bbscoin-pool.org:8107'],
+		['149.129.129.195', 'http://149.129.129.195:8117']
     ]
 };
