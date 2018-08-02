726bda7886dda58ba1763bce0bb1f9f602cecff3
bbscoindev <dev@bbscoin.xyz>
2018-08-01 21:54:25 -0700

    Add community pools

--- website/config.js
+++ website/config.js
@@ -21,12 +21,16 @@ var themeCss = "themes/default-theme.css";
 var networkStat = {
     "bbs": [
 		['pool.bbscoin.xyz', 'http://pool.bbscoin.xyz:8117'],
-		['bbs.dreampool.info', 'https://bbs.dreampool.info/api/'],
 		['cryptoknight.cc/bbs', 'http://78.46.85.142:2778'],
 		['bbs.gonspool.com', 'http://bbs.gonspool.com:8117'],
 		['pool-marine.ga/bbs', 'http://pool-marine.ga:8028'],
 		['bbs.howpool.com', 'http://bbs.howpool.com:8117'],
 		['bbscoin-pool.org', 'http://bbscoin-pool.org:8107'],
-		['149.129.129.195', 'http://149.129.129.195:8117']
+		['149.129.129.195', 'http://149.129.129.195:8117'],
+		['bbs.newpool.pw', 'http://bbs.newpool.pw:8122'],
+		['bbs.ucrypto.com', 'http://bbs.ucrypto.com:8117'],
+		['bbscoin.cryptohispano.net', 'https://bbscoin.cryptohispano.net/api'],
+		['bbs.codpool.com', 'https://pool1.codpool.com:8116'],
+		['bbs.minersunite.net', 'https://bbs.minersunite.net/api']
     ]
 };
